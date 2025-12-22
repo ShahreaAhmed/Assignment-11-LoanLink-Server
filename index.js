2d486501dbd9a770519095e8544ea07a1c65a720
Shahrea Ahmed <shahrearuman@gmail.com>
2025-12-22 21:25:48 +0600

    Admin Application Api Complete

--- index.js
+++ index.js
@@ -271,7 +271,3 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
